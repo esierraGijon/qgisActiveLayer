f825f2b350c0f4b6bac36dd1184ea37d60679b69
Enrique Sierra <74495818+esierraGijon@users.noreply.github.com>
2021-12-14 21:47:15 +0100

    Versión inicial

---
+++ __init__.py
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------
+# Copyright (C) 2015 Martin Dobias
+# -----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# ---------------------------------------------------------------------
+
+from PyQt5.QtWidgets import QAction, QMessageBox
+
+def classFactory(iface):
+    return MinimalPlugin(iface)
+
+class MinimalPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+        self.nombre = self.iface.activeLayer().name()
+
+    def initGui(self):
+        self.action = QAction('Go!', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+
+    def run(self):
+        self.iface.activeLayer().editingStarted.connect(self.inicioEdicion)
+        self.iface.activeLayer().editingStopped.connect(self.finEdicion)
+
+    def inicioEdicion(self):
+        QMessageBox.information(None, 'Minimal plugin', 'Comienza la edición de la capa ' + self.nombre)
+
+    def finEdicion(self):
+        QMessageBox.information(None, 'Minimal plugin', 'Finaliza la edición de la capa ' + self.nombre)
